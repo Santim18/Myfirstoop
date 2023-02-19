d38f5cdaf9759ab73866b285b37d2a3ad4bcf753
JOHAN SANTIAGO MARIN RIOS <johanmarin302678@correo.itm.edu.co>
2023-02-19 00:35:59 -0500

    I created my first code with a new Date Class

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try //Intenta hacer lo que esta por dentro si no funciona salta al catch capturando el error
+            {
+                var dateObject = new Date(2003, 2, 30);
+                Console.WriteLine(dateObject);
+            }
+            catch (Exception ex) //Message error 
+            {
+
+                Console.WriteLine (ex.Message);
+            }
+       
+
+        }
+    }
+}
