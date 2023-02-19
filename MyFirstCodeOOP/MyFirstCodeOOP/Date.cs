24d77c79e2d671c97d8a4382e525a53b5e1d27f4
JOHAN SANTIAGO MARIN RIOS <johanmarin302678@correo.itm.edu.co>
2023-02-19 12:51:40 -0500

    Insert ConsoleReadline for day, month and year

--- MyFirstCodeOOP/MyFirstCodeOOP/Date.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Date.cs
@@ -28,35 +28,30 @@ namespace MyFirstCodeOOP
 
         private int ValidateDay(int day, int month)
         {
-            if (month == 2)
-            {
-                if (day >= 1 && day <= 28)
-                {
-                    return day;
-                }
-                throw new DayException("The day is invalid");
-
-            }
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
             {
                 if (day >= 1 && day <= 31)
                 {
                     return day;
                 }
-
             }
-            else
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
             {
                 if (day >= 1 && day <= 30)
                 {
                     return day;
                 }
-
             }
-
+            else if (month == 2)
+            {
+                if (day >= 1 && day <= 28)
+                {
+                    return day;
+                }
+            }
             throw new DayException("The day is invalid");
-
         }
+   
 
         private int ValidateMonth(int month)
         {
--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -6,9 +6,14 @@ namespace MyFirstCodeOOP
     {
         static void Main(string[] args)
         {
+            int d = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
             try //Intenta hacer lo que esta por dentro si no funciona salta al catch capturando el error
             {
-                var dateObject = new Date(2003, 2, 30);
+                var dateObject = new Date(d,m,y);
                 Console.WriteLine(dateObject);
             }
             catch (Exception ex) //Message error 
